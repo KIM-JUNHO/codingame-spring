4dbc9f7436fa99d02d6eb630c964c66a64f32154
Unknown <prpr48@naver.com>
2021-05-14 20:14:19 +0900

    add Tree

--- source.java
+++ source.java
@@ -68,3 +68,18 @@ class Cell {
         this.neighbours = neighbours;
     }
 }
+
+class Tree {
+
+    int cellIndex;
+    int size;
+    boolean isMine;
+    boolean isDormant;
+
+    public Tree(int cellIndex, int size, boolean isMine, boolean isDormant) {
+        this.cellIndex = cellIndex;
+        this.size = size;
+        this.isMine = isMine;
+        this.isDormant = isDormant;
+    }
+}
