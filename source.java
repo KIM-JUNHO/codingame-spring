d19d4a47a3e04f902c28330af436a7f33bc12707
Unknown <prpr48@naver.com>
2021-05-14 20:15:10 +0900

    add Action

--- source.java
+++ source.java
@@ -83,3 +83,53 @@ class Tree {
         this.isDormant = isDormant;
     }
 }
+
+class Action {
+
+    static final String WAIT = "WAIT";
+    static final String SEED = "SEED";
+    static final String GROW = "GROW";
+    static final String COMPLETE = "COMPLETE";
+    String type;
+    Integer targetCellIdx;
+    Integer sourceCellIdx;
+
+    public Action(String type, Integer sourceCellIdx, Integer targetCellIdx) {
+        this.type = type;
+        this.targetCellIdx = targetCellIdx;
+        this.sourceCellIdx = sourceCellIdx;
+    }
+
+    public Action(String type, Integer targetCellIdx) {
+        this(type, null, targetCellIdx);
+    }
+
+    public Action(String type) {
+        this(type, null, null);
+    }
+
+    static Action parse(String action) {
+        String[] parts = action.split(" ");
+        switch (parts[0]) {
+            case WAIT:
+                return new Action(WAIT);
+            case SEED:
+                return new Action(SEED, Integer.valueOf(parts[1]), Integer.valueOf(parts[2]));
+            case GROW:
+            case COMPLETE:
+            default:
+                return new Action(parts[0], Integer.valueOf(parts[1]));
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (WAIT.equalsIgnoreCase(type)) {
+            return Action.WAIT;
+        }
+        if (SEED.equalsIgnoreCase(type)) {
+            return String.format("%s %d %d", SEED, sourceCellIdx, targetCellIdx);
+        }
+        return String.format("%s %d", type, targetCellIdx);
+    }
+}
